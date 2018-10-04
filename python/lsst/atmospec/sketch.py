528a3dae5ad8626a883149cc494bf84727286169
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2018-10-04 15:14:14 +0200

    Talking with Nick and Eli

--- python/lsst/atmospec/sketch.py
+++ python/lsst/atmospec/sketch.py
@@ -5,6 +5,11 @@ import lsst.afw.image as afwImage
 class Spectrum():
     """A (wavelength calibrated)"""
     # I don't think this is too heavyweight and could be useful over an ndarray
+
+    def __init__(self, spectrum2d, metadata):
+        # looks like np.zeros(1000, 100, dtype=float)
+        self.spectrum2d = spectrum2d
+
     spectrum2d = afwImage.maskedImage()
 
     # initially None as we haven't extracted/calibrated yet
@@ -25,31 +30,74 @@ class Spectrum():
     # Probably wants renaming as it's probably not really a chi^2
     chiSq = None
 
-    def __init__(self, spectrum2d):
-        # looks like np.zeros(1000, 100, dtype=float)
-        self.spectrum2d = spectrum2d
+    lsfOrPsf = None
 
-    def calibrate(self):
-        wl = WavelengthSolution()
-        wl.solve()
-        self.isCalibrated = True
-        self.wavelengthSolution = wl
+    skyBackground = None
+    skyBackgroundExtractionConfig = None
+    # skySpectrum = someAssumption?!
+
+    # no arclamp for auxTel?!
+    # need some way of getting the geomtric solution
+
+    # def calibrate(self):
+    # really this is probably done by a task, which creates the object
+    #     wl = WavelengthSolution()
+    #     wl.solve()
+    #     self.isCalibrated = True
+    #     self.wavelengthSolution = wl
+
+
+    def getWavelengthAtPixel(self, x, y):
+        return wavelength
+
+    def getPixelForWavelength(self, x, y):
+        return (x, y)
+        
+    def wavelengthAccuracyAtPixel():
+        return
+ 
+    def convolveWithKernel():
+        return
+        
+    def resample():
+        return
+       
+    def getColumnwiseFit():
+        return columnSlice, fit, chiSq
+
+    def calculateEquivalentWidth(line):
+        return width
+
 
 
 class WavelengthSolution():
+    # assume self-calibrating from lines as we have no arc lamp
+    # starts from the geometric solution and refines from there
+    # how stable is this? Can we use the nominal solution to find the Balmer series
+    # and then solve using that
+
+    # need to fit a continuum first and then fit lines on top of that
+
     # position of the source centroid on the CCD as floats
     sourceLocation = [x, y]
-
+    orderBlockingFilters = []
+    gratingType = ['RONCHI400', 'HOLOGRAPHIC_MADNESS', 'etc']
     directImageSubtracted = [True, False]
     visitInfo = "expTime, RA/Dec, objectId of source etc"
+
+    # we need to know where it originally hit the detector
+    # so that we can redo flatfielding
+    detectorLocation = [x, y]
+
 
     def solve(self):
         """Find the wavelength solution"""
         return self
 
-    def getWavelengthAtPixel(self, x, y):
-        wavelength = 0
-        return wavelength
 
-    def getPixelForWavelength(self, x, y):
-        return (x, y)
+
+
+
+
+
+
