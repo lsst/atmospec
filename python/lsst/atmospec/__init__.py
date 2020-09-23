ddc411414ba3c37ed5c849ad9e86839a35e8b60e
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2020-09-23 18:44:41 -0500

    Remove unused import that crept in

--- python/lsst/atmospec/__init__.py
+++ python/lsst/atmospec/__init__.py
@@ -23,5 +23,6 @@ import pkgutil
 import lsstimport
 from .processStar import *
 from .dispersion import *
+from .utils import *
 from .version import *
 __path__ = pkgutil.extend_path(__path__, __name__)
