b8b1ccda89ba60a145f37f46d7398d7d78be99c7
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2021-07-06 16:48:11 -0500

    Remove comments to self

--- python/lsst/atmospec/processStar.py
+++ python/lsst/atmospec/processStar.py
@@ -243,8 +243,8 @@ class ProcessStarTaskConfig(pipeBase.PipelineTaskConfig,
 
         self.charImage.doApCorr = False
         self.charImage.doMeasurePsf = False
-        self.charImage.repair.cosmicray.nCrPixelMax = 100000  # XXX this is new!
-        self.charImage.repair.doCosmicRay = False  # XXX this is new!
+        self.charImage.repair.cosmicray.nCrPixelMax = 100000
+        self.charImage.repair.doCosmicRay = False
         if self.charImage.doMeasurePsf:
             self.charImage.measurePsf.starSelector['objectSize'].signalToNoiseMin = 10.0
             self.charImage.measurePsf.starSelector['objectSize'].fluxMin = 5000.0
