d8596e797107a3b1f979d2db441ad850cee734b9
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2023-10-31 08:13:55 -0700

    Keep PSF regularization at 1

--- python/lsst/atmospec/processStar.py
+++ python/lsst/atmospec/processStar.py
@@ -356,7 +356,7 @@ class ProcessStarTaskConfig(pipeBase.PipelineTaskConfig,
         dtype=float,
         doc="Regularisation parameter for the chisq minimisation to extract the spectrum. "
         "PSF_FIT_REG_PARAM internally.",
-        default=.1,
+        default=1,
         # XXX allowed range strictly positive
     )
     psfTransverseStepSize = pexConfig.Field(
